52e17a9e9d602a6929da0109df9edd63b91d5cbe
Tuhin Roy <tuhinroy881@gmail.com>
2022-04-20 19:08:02 +0530

    wheel try with different package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_conditional_assignment, unnecessary_null_comparison, curly_braces_in_flow_control_structures, unused_element, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_conditional_assignment, unnecessary_null_comparison, curly_braces_in_flow_control_structures, unused_element, prefer_typing_uninitialized_variables, unrelated_type_equality_checks
 
 import 'package:flutter/material.dart';
 import 'package:graphic/graphic.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Color> palette = const <Color>[
+    Color.fromRGBO(75, 135, 185, 1),
+    Color.fromRGBO(192, 108, 132, 1),
+    Color.fromRGBO(246, 114, 128, 1),
+    Color.fromRGBO(248, 177, 149, 1),
+    Color.fromRGBO(116, 180, 155, 1),
+    Color.fromRGBO(0, 168, 181, 1),
+    Color.fromRGBO(73, 76, 162, 1),
+    Color.fromRGBO(255, 205, 96, 1),
+    Color.fromRGBO(255, 240, 219, 1),
+    Color.fromRGBO(238, 238, 238, 1)
+  ];
   String selectedValue = "1";
   var user_input_chart_value1 = "0";
   var user_input_chart_value2 = "0";
@@ -147,101 +160,207 @@ class _MyHomePageState extends State<MyHomePage> {
                           "assets/images/Wheel.png",
                         )),
                     Positioned(
-                        top: 19,
-                        right: 63,
+                        top: 28,
+                        right: 90,
                         child: Transform(
                           transform: Matrix4.rotationZ(
                               0.320000), //===============================Transform to rotated the graph
                           alignment: FractionalOffset.center,
-                          child: Container(
-                            margin: const EdgeInsets.only(top: 10),
-                            width: 350,
-                            height: 302,
-                            child: Chart(
-                              data: [
-                                {
-                                  'value': int.parse(user_input_chart_value1),
-                                  'name':
-                                      '/' //################################# P R O B L E M
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value2),
-                                  'name': '.'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value3),
-                                  'name': '..'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value4),
-                                  'name': '.*'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value5),
-                                  'name': 'o.'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value6),
-                                  'name': ':'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value7),
-                                  'name': '._'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value8),
-                                  'name': '.i'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value9),
-                                  'name': '^'
-                                },
-                                {
-                                  'value': int.parse(user_input_chart_value10),
-                                  'name': '.~'
-                                },
-                              ],
-                              variables: {
-                                'name': Variable(
-                                  accessor: (Map map) => map['name'] as String,
-                                ),
-                                'value': Variable(
-                                  accessor: (Map map) => map['value'] as num,
-                                  scale: LinearScale(min: 0, marginMax: 0.1),
-                                ),
-                              },
-                              elements: [
-                                IntervalElement(
-                                  label: LabelAttr(
-                                      encoder: (tuple) =>
-                                          Label(tuple['name'].toString())),
-                                  shape: ShapeAttr(
-                                      value: RectShape(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(1)),
-                                  )),
-                                  color: ColorAttr(variable: 'name', values: [
-                                    Color(0xff5b8ff9).withOpacity(0.3),
-                                    Color(0xff5ad8a6).withOpacity(0.3),
-                                    Color(0xff5d7092).withOpacity(0.3),
-                                    Color(0xfff6bd16).withOpacity(0.3),
-                                    Color(0xff6f5ef9).withOpacity(0.3),
-                                    Color(0xff6dc8ec).withOpacity(0.3),
-                                    Color(0xff945fb9).withOpacity(0.3),
-                                    Color(0xffff9845).withOpacity(0.3),
-                                    Color(0xff1e9493).withOpacity(0.3),
-                                    Color(0xffff99c3).withOpacity(0.3),
-                                  ]),
-                                  elevation: ElevationAttr(value: 0),
-                                )
-                              ],
-                              coord: PolarCoord(startRadius: 0.15),
-                              // axes: [
-                              //   Defaults.circularAxis,
-                              //   Defaults.radialAxis..label = null,
-                              // ],
-                            ),
+                          child: SfCircularChart(
+                            //backgroundColor: Colors.red,
+                            palette: <Color>[
+                              Color(0xffE5C7A0),
+                              Colors.brown.withOpacity(0.3),
+                              Colors.green.withOpacity(0.3),
+                              Colors.redAccent.withOpacity(0.3),
+                              Colors.blueAccent.withOpacity(0.3),
+                              Colors.teal.withOpacity(0.3)
+                            ],
+                            series: <DoughnutSeries<ChartData, String>>[
+                              DoughnutSeries<ChartData, String>(
+                                //opacity: 100,
+                                radius: '30%',
+                                dataSource: <ChartData>[
+                                  //   ChartData(
+                                  //       'USA',
+                                  //       9,
+                                  //       (int.parse(user_input_chart_value1) - 5)
+                                  //               .toString() +
+                                  //           '0%'),
+                                  // ChartData(
+                                  //     'China',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value2) - 5)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Russia',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value3) - 5)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Germany',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value4) - 5)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'USA',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value5) - 5)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'China',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value7) - 6)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Russia',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value8) - 7)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Germany',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value9) - 8)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Russia',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value1) - 9)
+                                  //             .toString() +
+                                  //         '0%'),
+                                  // ChartData(
+                                  //     'Germany',
+                                  //     9,
+                                  //     (int.parse(user_input_chart_value1) - 10)
+                                  //             .toString() +
+                                  //         '0%')
+                                  //===========================================================
+                                  ChartData(
+                                      'USA', 9, user_input_chart_value1 + '0%'),
+                                  ChartData('China', 9,
+                                      user_input_chart_value2 + '0%'),
+                                  ChartData('Russia', 9,
+                                      user_input_chart_value3 + '0%'),
+                                  ChartData('Germany', 9,
+                                      user_input_chart_value4 + '0%'),
+                                  ChartData(
+                                      'USA', 9, user_input_chart_value5 + '0%'),
+                                  ChartData('China', 9,
+                                      user_input_chart_value6 + '0%'),
+                                  ChartData('Russia', 9,
+                                      user_input_chart_value7 + '0%'),
+                                  ChartData('Germany', 9,
+                                      user_input_chart_value8 + '0%'),
+                                  ChartData('Russia', 9,
+                                      user_input_chart_value9 + '0%'),
+                                  ChartData('Germany', 9,
+                                      user_input_chart_value10 + '0%')
+                                ],
+                                xValueMapper: (ChartData data, _) => data.xVal,
+                                yValueMapper: (ChartData data, _) => data.yVal,
+                                pointRadiusMapper: (ChartData data, _) =>
+                                    data.radius,
+                              ),
+                            ],
                           ),
+                          // Container(
+                          //   margin: const EdgeInsets.only(top: 10),
+                          //   width: 350,
+                          //   height: 302,
+                          //   child: Chart(
+                          //     data: [
+                          //       {
+                          //         'value': int.parse(user_input_chart_value1),
+                          //         'name':
+                          //             '/' //################################# P R O B L E M
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value2),
+                          //         'name': '.'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value3),
+                          //         'name': '..'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value4),
+                          //         'name': '.*'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value5),
+                          //         'name': 'o.'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value6),
+                          //         'name': ':'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value7),
+                          //         'name': '._'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value8),
+                          //         'name': '.i'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value9),
+                          //         'name': '^'
+                          //       },
+                          //       {
+                          //         'value': int.parse(user_input_chart_value10),
+                          //         'name': '.~'
+                          //       },
+                          //       // {'value': 10, 'name': 'adad'},
+                          //     ],
+                          //     variables: {
+                          //       'name': Variable(
+                          //         accessor: (Map map) => map['name'] as String,
+                          //       ),
+                          //       'value': Variable(
+                          //         accessor: (Map map) => map['value'] as num,
+                          //         scale: LinearScale(min: 0, marginMax: 0.1),
+                          //       ),
+                          //     },
+                          //     elements: [
+                          //       IntervalElement(
+                          //         label: LabelAttr(
+                          //             encoder: (tuple) =>
+                          //                 Label(tuple['name'].toString())),
+                          //         shape: ShapeAttr(
+                          //             value: RectShape(
+                          //           borderRadius: const BorderRadius.all(
+                          //               Radius.circular(1)),
+                          //         )),
+                          //         color: ColorAttr(variable: 'name', values: [
+                          //           Color(0xff5b8ff9).withOpacity(0.3),
+                          //           Color(0xff5ad8a6).withOpacity(0.3),
+                          //           Color(0xff5d7092).withOpacity(0.3),
+                          //           Color(0xfff6bd16).withOpacity(0.3),
+                          //           Color(0xff6f5ef9).withOpacity(0.3),
+                          //           Color(0xff6dc8ec).withOpacity(0.3),
+                          //           Color(0xff945fb9).withOpacity(0.3),
+                          //           Color(0xffff9845).withOpacity(0.3),
+                          //           Color(0xff1e9493).withOpacity(0.3),
+                          //           Color(0xffff99c3).withOpacity(0.3),
+                          //           //Colors.transparent
+                          //         ]),
+                          //         elevation: ElevationAttr(value: 0),
+                          //       )
+                          //     ],
+                          //     coord: PolarCoord(startRadius: 0.15),
+                          //     // axes: [
+                          //     //   Defaults.circularAxis,
+                          //     //   Defaults.radialAxis..label = null,
+                          //     // ],
+                          //   ),
+                          // ),
                         )),
                   ]),
                 ),
@@ -253,4 +372,11 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.xVal, this.yVal, this.radius);
+  final String xVal;
+  final int yVal;
+  final String radius;
 }
